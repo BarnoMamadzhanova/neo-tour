178550a8d0590cd5845eca2e9941e224b6622198
BarnoMamadzhanova <b@mamadzhanova.com>
2024-06-18 22:23:08 +0200

    fixed stzle issues for active categories, and for images in carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,20 +1,28 @@
+import React, { forwardRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Navigation, Pagination, A11y } from "swiper/modules";
 import "swiper/swiper-bundle.css";
 import classes from "./Carousel.module.css";
 
-export const Carousel = ({ slides }) => {
+export const Carousel = forwardRef(({ slides }, ref) => {
   return (
     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      ref={ref}
+      modules={[Navigation, Pagination, A11y]}
       spaceBetween={24}
       slidesPerView={3}
-      navigation
     >
       {slides.map((slide) => (
         <SwiperSlide key={slide.id}>
-          <div className={classes.slide__box}>
-            <img src={slide.image} alt={slide.title} />
+          <div
+            className={classes.slide__box}
+            style={{ backgroundImage: `url(${slide.image})` }}
+          >
+            {/* <img
+              src={slide.image}
+              alt={slide.title}
+              className={classes.slide__image}
+            /> */}
             <div className={classes.slider__title_bg}>
               <h3>{slide.title}</h3>
             </div>
@@ -23,4 +31,4 @@ export const Carousel = ({ slides }) => {
       ))}
     </Swiper>
   );
-};
+});
--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./Discover.module.css";
 import slides from "../../mock.json";
 import categories from "../../categories.json";
@@ -8,6 +8,35 @@ import prev from "../../assets/prevBtn.svg";
 import next from "../../assets/nextBtn.svg";
 
 function Discover() {
+  const swiperRef = useRef(null);
+
+  const handlePrevClick = () => {
+    swiperRef.current.swiper.slidePrev();
+  };
+
+  const handleNextClick = () => {
+    swiperRef.current.swiper.slideNext();
+  };
+
+  //   const handleSlideChange = (swiper) => {
+  //     document.querySelectorAll(`.${classes.slide__image}`).forEach((img) => {
+  //       img.style.border = "none";
+  //     });
+  //     // Add border to the active slide
+  //     const activeSlide = swiper.slides[swiper.activeIndex].querySelector(
+  //       `.${classes.slide__image}`
+  //     );
+  //     if (activeSlide) {
+  //       activeSlide.style.border = "1px solid black";
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     if (swiperRef.current && swiperRef.current.swiper) {
+  //       handleSlideChange(swiperRef.current.swiper);
+  //     }
+  //   }, []);
+
   return (
     <section className={classes.discover}>
       <div className={classes.container}>
@@ -15,23 +44,17 @@ function Discover() {
           <h2>Discover</h2>
           <div className={classes.navigation__btns}>
             <button
-              // onClick={() => {
-              //   swiperRef.current?.slidePrev();
-              // }}
+              onClick={handlePrevClick}
               type="button"
               data-role="none"
-              className="carousel-topNav-prev slick-custom-buttons"
               aria-label="Previous"
             >
               <img src={prev} alt="Previous" />
             </button>
             <button
-              // onClick={() => {
-              //   swiperRef.current?.slideNext();
-              // }}
+              onClick={handleNextClick}
               type="button"
               data-role="none"
-              className="carousel-topNav-next slick-custom-buttons"
               aria-label="Next"
             >
               <img src={next} alt="Next" />
@@ -40,7 +63,7 @@ function Discover() {
         </div>
 
         <Categories categories={categories} />
-        <Carousel slides={slides} />
+        <Carousel ref={swiperRef} slides={slides} />
       </div>
     </section>
   );

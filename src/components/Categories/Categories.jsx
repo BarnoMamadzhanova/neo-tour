6c08ae82f067029ee9c93279c888fbdea6b58dc5
BarnoMamadzhanova <b@mamadzhanova.com>
2024-06-19 22:29:53 +0200

    fixed selected categories style - added active class according to design

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Categories.module.css";
 
 export const Categories = ({ categories = [], onCategoryClick }) => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category.title);
+    onCategoryClick(category.path);
+  };
+
   return (
     <>
       {categories && categories.length > 0 ? (
         <ul className={classes.list}>
           {categories.map((category) => (
-            <li key={category.title}>
-              <button
-                className={classes.link}
-                onClick={() => onCategoryClick(category.path)}
-              >
-                {category.title}
-              </button>
+            <li
+              key={category.title}
+              className={`${classes.link} ${
+                selectedCategory === category.title ? classes.active : ""
+              }`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              <p>{category.title}</p>
             </li>
           ))}
         </ul>
